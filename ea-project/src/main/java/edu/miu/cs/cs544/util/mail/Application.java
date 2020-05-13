0002dbc18036e2c41e004822f6689b40156e1487
birhanegg <59857947+birhanegg@users.noreply.github.com>
2020-05-13 17:13:13 -0500

    email and cron done

---
+++ ea-project/src/main/java/edu/miu/cs/cs544/util/mail/Application.java
@@ -0,0 +1,22 @@
+
+package edu.miu.cs.cs544.util.mail;
+
+import javax.mail.MessagingException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// @Override
+	public void run(String... args) throws MessagingException {
+		System.out.println("Appointment Status-Tracker: STARTED");
+	}
+}
