9c858f05bb42ca7a07bec79962bf7ac3e0f34c1e
birhanegg <59857947+birhanegg@users.noreply.github.com>
2020-05-14 01:56:14 -0500

    reservation and scheduler connection

--- ea-project/src/main/java/edu/miu/cs/cs544/util/mail/CronJob.java
+++ ea-project/src/main/java/edu/miu/cs/cs544/util/mail/CronJob.java
@@ -1,10 +1,50 @@
+/*
+ * By Birhane Geber
+ * Since : May 2020
+ * 
+ *  Description: Cron based Scheduler for reserveration email sending and remainder notification 
+ *  from database table 
+ * */
+
 package edu.miu.cs.cs544.util.mail;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CronJob {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	@Value("${spring.mail.username}")
+	private String from;
+
+	public enum STATUS {
+		PENDING, APPROVED, DECLINE, HANDLED
+	}
+
+	public void setMailSender(JavaMailSender javaMailSender) {
+
+		this.javaMailSender = javaMailSender;
+	}
+
+	public JavaMailSender getJavaMailSender() {
+		return javaMailSender;
+	}
+
+	public void setJavaMailSender(JavaMailSender javaMailSender) {
+		this.javaMailSender = javaMailSender;
+	}
 
 	/**
 	 * trigger every 10 seconds for email response for accept/reject reservation
@@ -12,16 +52,53 @@ public class CronJob {
 
 	@Scheduled(cron = "0/10 * * * * *")
 	public void emailAppointmentStatus() {
-		// connect db table, parse and send
+
+		Connection connection = null;
+		Statement insertStmt = null;
+		Statement selectStmt = null;
+
+		System.out.println("Table contains ows");
 
 		try {
-			Email.notificationAppointmentStatus();
+
+			System.out.println("inside try ");
+
+			StringBuffer sb = new StringBuffer();
+			Connection con = getConnection();
+			selectStmt = con.createStatement();
+			ResultSet rs = selectStmt.executeQuery("SELECT u.firstname, u.lastname,u.email,r.dateAndTime "
+					+ "FROM reservation r JOIN user u " + "on r.consumer_id=u.id and r.status=2");
+
+			while (rs.next()) {
+
+				System.out.println("Email processing .... ");
+
+				String firstname = rs.getString(1);
+				String lastname = rs.getString(2);
+				String email = rs.getString(3);
+				String dateAndTime = rs.getString(4);
+
+				String to = email;
+				String subject = "your Appointment Approved";
+				String body = " Dear " + firstname + " " + lastname + ",";
+				body += " your Appointment  on " + dateAndTime + " is Approved";
+
+				SimpleMailMessage emailMessage = new SimpleMailMessage();
+
+				emailMessage.setFrom(from);
+				emailMessage.setTo(to);
+				emailMessage.setSubject(subject);
+				emailMessage.setText(body);
+
+				javaMailSender.send(emailMessage);
+
+			}
 
 		} catch (Exception e) {
+			// TODO: handle exception
+
 			e.getStackTrace();
 		}
-
-		System.out.println("emailAppointmentStatus():  run every 10 seconds ");
 	}
 
 	/**
@@ -30,18 +107,33 @@ public class CronJob {
 	 * @Scheduled(cron = "15 * * * * * MON-FRI") //for testing
 	 */
 
-	@Scheduled(cron = "0 0 10 ? * MON-FRI")
-
+	// @Scheduled(cron = "0 0 10 ? * MON-FRI")
 	public void emailRemainder() {
 
 		try {
-			Email.appointmentReminder();
+			Email email = new Email();
+			email.appointmentReminder();
+			System.out.println("Email.appointmentReminder():  run every Day at 10:00 AM to Remaind Students ");
 
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.getStackTrace();
 		}
 
+	}
+
+	public static Connection getConnection() {
+		Connection con = null;
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/cs544", "ea", "ea123");
+
+		} catch (Exception e) {
+
+			System.out.println(e);
+		}
+		return con;
 	}
 
 }
--- ea-project/src/main/java/edu/miu/cs/cs544/util/mail/Email.java
+++ ea-project/src/main/java/edu/miu/cs/cs544/util/mail/Email.java
@@ -2,28 +2,28 @@ package edu.miu.cs.cs544.util.mail;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Email {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
 
+	public enum STATUS {
+		PENDING, APPROVED, DECLINE
+	}
+
 	public void setMailSender(JavaMailSender javaMailSender) {
 
 		this.javaMailSender = javaMailSender;
-
 	}
 
 	public static Connection getConnection() {
@@ -41,33 +41,56 @@ public class Email {
 		return con;
 	}
 
-	public static void notificationAppointmentStatus() throws SQLException {
+	public void notificationAppointmentStatus() throws SQLException {
 
 		Connection connection = null;
 		Statement insertStmt = null;
 		Statement selectStmt = null;
 
+		System.out.println("Table contains ows");
+
 		try {
+
+			System.out.println("inside try ");
 
 			StringBuffer sb = new StringBuffer();
 			Connection con = getConnection();
 			selectStmt = con.createStatement();
-			ResultSet rs = selectStmt.executeQuery("SELECT * FROM email  where email_status='pending'");
-
-			System.out.println("Table contains " + rs.getInt("count(*)") + " rows");
+			ResultSet rs = selectStmt.executeQuery("SELECT u.firstname, u.lastname,u.email,r.dateAndTime "
+					+ "FROM reservation r JOIN user u " + "on r.consumer_id=u.id and r.status=2");
 
 			while (rs.next()) {
 
 				System.out.println("Email processing .... ");
 
-				sb.append("ID: " + rs.getString(1));
-				sb.append("From: " + rs.getString(2));
-				sb.append("To : " + rs.getString(3));
-				sb.append("Subject: " + rs.getString(4));
-				sb.append("Body: " + rs.getString(5));
-				sb.append("Date : " + rs.getString(6));
-				sb.append("status : " + rs.getString(7));
-				// System.out.println(sb.toString());
+				String firstname = rs.getString(1);
+				String lastname = rs.getString(2);
+				String email = rs.getString(3);
+				String dateAndTime = rs.getString(4);
+
+				String from = "birg20@gmail.com";
+				String to = email;
+				String subject = "your Appointment Approved";
+				String body = " Dear " + firstname + " " + lastname + ",";
+				body += " your Appointment  on " + dateAndTime + " is Approved";
+
+				System.out.println("from:  " + from);
+				System.out.println("to:  " + to);
+				System.out.println("subject: " + subject);
+				System.out.println("body:  " + body);
+
+				sendEmail(from, to, subject, body);
+
+				SimpleMailMessage msg = new SimpleMailMessage();
+				msg.setFrom("birg20@gmail.com");
+				msg.setTo("birg2000@gmail.com");
+				msg.setSubject("Testing from Spring Boot");
+				msg.setText("Hello World \n Spring Boot Email");
+
+				// javaMailSender.send(msg);
+
+				// javaMailSender.send(simpleMailMessage);
+				System.out.println("Email sent to " + email);
 			}
 
 		} catch (Exception e) {
@@ -76,7 +99,7 @@ public class Email {
 		}
 	}
 
-	public static void appointmentReminder() throws SQLException {
+	public void appointmentReminder() throws SQLException {
 
 		Connection connection = null;
 		Statement insertStmt = null;
@@ -87,7 +110,8 @@ public class Email {
 			StringBuffer sb = new StringBuffer();
 			Connection con = getConnection();
 			selectStmt = con.createStatement();
-			ResultSet rs = selectStmt.executeQuery("SELECT * FROM email  where email_status='pending'");
+			ResultSet rs = selectStmt.executeQuery(
+					"SELECT u.firstname, u.lastname, u.email FROM reservation r JOIN user u on r.consumer_id=u.id and r.status=2");
 
 			System.out.println("Table contains " + rs.getInt("count(*)") + " rows");
 
@@ -102,7 +126,8 @@ public class Email {
 				sb.append("Body: " + rs.getString(5));
 				sb.append("Date : " + rs.getString(6));
 				sb.append("status : " + rs.getString(7));
-				// System.out.println(sb.toString());
+
+				System.out.println(sb.toString());
 			}
 
 		} catch (Exception e) {
@@ -119,32 +144,6 @@ public class Email {
 		simpleMailMessage.setTo(to);
 		simpleMailMessage.setSubject(subject);
 		simpleMailMessage.setText(body);
-
-		// insert into db then use cron to send Asyn. way
-		try {
-
-			Connection con = getConnection();
-			PreparedStatement ps = con.prepareStatement(
-					"insert into email(email_id,email_from,email_to,email_subject,email_body,email_date,email_status) values(?,?,?,?,?,?,?)");
-
-			Date date = Calendar.getInstance().getTime();
-			DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-			String strDate = dateFormat.format(date);
-
-			ps.setString(1, null);
-			ps.setString(2, from);
-			ps.setString(3, to);
-			ps.setString(4, subject);
-			ps.setString(5, body);
-			ps.setString(6, strDate);
-			ps.setString(7, "pending");
-
-			int i = ps.executeUpdate();
-			System.out.println(i + " records inserted");
-			con.close();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
 
 		javaMailSender.send(simpleMailMessage);
 	}
